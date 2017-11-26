081199923a6d31719bbe4570cd1d3868f25fa67d
Sofiane Medjkoune <sofiane@medjkoune.fr>
2017-11-26 05:16:34 +0100

    Fix shell scripts and lxd.yml

--- test.sh
+++ test.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-ROLE_DIR="${DIR}/roles/role-to-test"
-TEST_DIR="${ROLE_DIR}/tests"
-
+# Display functions
 function message()
 {
     COLOR="${1}"
-    MESSAGE="${2}"
+    shift
+    MESSAGE="${@}"
     echo -e "${COLOR}${MESSAGE}${NORMAL}\n"
 }
-
 function execution_message()
 {
     message "${GREEN}" "Executing : ${@}"
 }
+
+# Set dir vars
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+ROLE_DIR="${DIR}/roles/role-to-test"
+TEST_DIR="${ROLE_DIR}/tests"
+
+# Set colors.
+NORMAL=$'\E(B\E[m'
+RED=$'\E[31m'
+GREEN=$'\E[32m'
 
 # Setting up the test environment
 execution_message ansible-playbook "${DIR}/setup.yml"
